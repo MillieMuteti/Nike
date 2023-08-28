7ade709618cfb52b9ed5c393f43a469c33ac2b71
Millie Muteti <milliemuteti@gmail.com>
2023-08-28 21:56:59 +0300

    setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+const App = () => {
+  return (
+    <main className='relative'>
+      Nav
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        Hero
+      </section>
+      <section className="padding">
+        Popular Products
+      </section>
+
+      <section className="padding">
+        Super Quality Products
+      </section>
+
+      <section className="padding">
+        Services
+      </section>
+
+      <section className="padding">
+       SpecialOffers
+      </section>
+    </main>
+  )
+}
+
+export default App
