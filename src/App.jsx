aa220af357ae28cb7ac41914858d3710f566e97a
Millie Muteti <milliemuteti@gmail.com>
2023-08-28 22:05:23 +0300

    build up the app section

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,24 @@ const App = () => {
         Super Quality Products
       </section>
 
-      <section className="padding">
+      <section className="padding-x py-10">
         Services
       </section>
 
       <section className="padding">
        SpecialOffers
+      </section>
+
+      <section className="padding bg-pale-blue">
+        CustomerReviews
+      </section>
+
+      <section className="padding-x sm:py-32 py-16 w-full">
+       Subscription
+      </section>
+
+      <section className="bg-black padding-x padding-t pb-8">
+       Footer
       </section>
     </main>
   )
