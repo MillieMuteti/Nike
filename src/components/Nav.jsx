ba5ac9fd11a3ce8bc4053e6dec44fe111633b19c
Millie Muteti <milliemuteti@gmail.com>
2023-08-29 15:20:38 +0300

    add  hamburger

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,6 +12,7 @@ const Nav = () => {
         width={130}
         height={29}
         />
+        </a>
         <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
           {navLinks.map((item) => (
             <li key={item.label}>
@@ -22,8 +23,15 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-
-      </a>
+        <div className='hidden max-lg:block'>
+          <img
+          src={hamburger}
+          alt=''
+          width={25}
+          height={25}
+          />
+        </div>
+      
     </nav>
    </header>
   )
